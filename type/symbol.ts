f3904c5a88fc08f1a0b9970e10201eaea4a279eb
Aiyern30 <ianbian2@gmail.com>
2024-12-17 14:54:06 +0800

    created possible enum for strong typescript

--- type/symbol.ts
+++ type/symbol.ts
@@ -2,3 +2,37 @@ export enum Temperature {
   DEGREE = "DEGREE",
   FAHRENHEIT = "FAHRENHEIT",
 }
+
+export enum WeatherCondition {
+  RAIN = "RAIN",
+  SNOW = "SNOW",
+  WIND = "WIND",
+  CLOUDY = "CLOUDY",
+  CLEAR = "CLEAR",
+}
+
+export enum TemperatureType {
+  FEELS_LIKE = "feelslike",
+  WINDCHILL = "windchill",
+  HEAT_INDEX = "heatindex",
+  DEWPOINT = "dewpoint",
+}
+
+export enum PressureUnit {
+  MB = "mb", // Millibar
+  INCH = "in", // Inch of mercury
+}
+
+export enum PrecipitationUnit {
+  MM = "mm", // Millimeters
+  INCH = "in", // Inches
+}
+
+export enum DistanceUnit {
+  MPH = "mph", // Miles per hour
+  KPH = "kph", // Kilometers per hour
+}
+
+export enum HumidityUnit {
+  PERCENT = "percentage", // Percentage representation of humidity
+}
